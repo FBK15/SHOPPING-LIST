d570ed04b4d78ebd882e0b0a19b5cf5ecf12b184
Asadilhaq Elqudsi Prabowo <asadilhaqklem@gmail.com>
2023-09-06 14:01:15 +0700

    penambahan django test di shopping list proyek

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name': 'Asadilhaq Elqudsi Prabowo',
+        'class': 'PBP B'
+    }
+
+    return render(request, "main.html", context)
+# Create your views here.
